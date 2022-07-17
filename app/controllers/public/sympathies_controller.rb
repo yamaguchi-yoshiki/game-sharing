eebce2d7bf3cd3fcb9391b9a77363e8acb2aff73
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-07-17 16:01:55 +0000

    [Add] レビューいいね機能

--- app/controllers/public/sympathies_controller.rb
+++ app/controllers/public/sympathies_controller.rb
@@ -1,14 +1,14 @@
 class Public::SympathiesController < ApplicationController
-  def update
-    review = review.find(params[:review_id])
-    sympathy = current_customer.sympathies.new(review_id: review.id)
+  def create
+    @review = Review.find(params[:review_id])
+    sympathy = current_customer.sympathies.new(review_id: @review.id)
     sympathy.save
     redirect_to request.referer
   end
 
   def destroy
-    review = review.find(params[:review_id])
-    sympathy = current_customer.sympathies.find_by(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    sympathy = current_customer.sympathies.find_by(review_id: @review.id)
     sympathy.destroy
     redirect_to request.referer
   end
